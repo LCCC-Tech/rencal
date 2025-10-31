1d8ff1309e8cf4fb1c4552e9e4eecd9bfd3c2244
imharrisonking <harrison.k@hotmail.co.uk>
2025-10-31 19:12:39 +0000

    Completes data_downloader module for raw downloading of public datasets

--- weather/core/constants.py
+++
@@ -1,6 +0,0 @@
-# Date related
-ERA5_START_YEAR = 2016
-DAYS_IN_YEAR = 365
-
-# Blob storage
-CONTAINER_NAME = "dw-bronze"
--- weather/main.py
+++ weather/main.py
@@ -3,8 +3,9 @@ from weather.core.data_downloader import DownloadManager
 
 def main():
     downloader = DownloadManager()
-    downloader.download_era5()
     downloader.download_cfd()
+    downloader.download_generation_data()
+    downloader.download_era5()
 
 
 if __name__ == "__main__":
