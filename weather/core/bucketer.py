14d14f09d16507505e1e388bb2ce38c0aa0da172
imharrisonking <harrison.k@hotmail.co.uk>
2025-11-07 13:44:24 +0000

    Updates bucketer with formatting

--- weather/core/bucketer.py
+++ weather/core/bucketer.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class Bucketer:
     """
     The Bucketer deals with certain sampling for our simulation engine to build future data timeseries from past data.
@@ -21,14 +22,17 @@ class Bucketer:
         _lookup_Structure (dict): A dictionary of all dates in the bucket structure, each with a tabulated date information.
 
     """
-    def __init__(self, historical_start_date, historical_end_date, categories, draw_period, bucket_definition):
+
+    def __init__(
+        self, historical_start_date, historical_end_date, categories, draw_period, bucket_definition
+    ):
         """
         In initializing a Bucketer, we build a hierarchical data structure called the bucket_Structure that is the core of this class.
         The bucket_Structure contains all individual calendar dates between the historical_Start_Date and historical_End_Date inclusive, each filtered through 2 layers of logic, ``bucket_of()`` and ``assign_Category()``, that assign it to a particular "category" and "bucket".
 
         - The "category" is a placeholder descriptor of the date for when children classes need an additional level of sieving.
         - The "bucket" is refering to the position of that date in the ``bucket_Definition``, a custom partition of the year specified by an ordered list of :class:`ShortDate` objects (calendar month/day pairs).
-        
+
         These are both represented by consecutive integers starting at ``0``.
 
         Args:
@@ -39,7 +43,7 @@ class Bucketer:
             bucket_definition (list): A sorted list of :class:`ShortDates` that completely partitions the year into buckets, each bucket being represented by the range of dates between two :class:`ShortDates`.
         """
         if not historical_start_date <= historical_end_date:
-            raise ValueError("Historical dates are not ordered") 
+            raise ValueError("Historical dates are not ordered")
         # A datatime.datetime object containing the midnight (start) of the first day for which we have historical data available
         self._historical_Start_Date = historical_start_date
         # A datatime.datetime object containing the midnight (start) of the last day for which we have historical data available
@@ -52,17 +56,30 @@ class Bucketer:
             raise TypeError("The bucket definition provided is not a list")
         if not len(bucket_definition) >= 2:
             raise ValueError("The bucket definition does not define a proper partition of a year")
-        if not all(bucket_definition[i] < bucket_definition[i + 1] for i in range(len(bucket_definition) - 1)):
-            raise ValueError("The bucket definition is not strictly ordered so does not define mutually exclusive year partitions")
-        if not all((bucket_definition[i + 1] - bucket_definition[i]) >= draw_period for i in range(len(bucket_definition) - 1)):
-            raise ValueError("There is a bucketed interval that is smaller that the days we are drawing by")
+        if not all(
+            bucket_definition[i] < bucket_definition[i + 1]
+            for i in range(len(bucket_definition) - 1)
+        ):
+            raise ValueError(
+                "The bucket definition is not strictly ordered so does not define mutually exclusive year partitions"
+            )
+        if not all(
+            (bucket_definition[i + 1] - bucket_definition[i]) >= draw_period
+            for i in range(len(bucket_definition) - 1)
+        ):
+            raise ValueError(
+                "There is a bucketed interval that is smaller that the days we are drawing by"
+            )
         # Continuous days sampled at once starting from the date pooled by each iteration of the loop in random_Sample()
         self._draw_Period = draw_period
         # A sorted list of ShortDates that completely partitions the year into buckets, each bucket being represented by the range of dates between 2 ShortDates
         self._bucket_Definition = bucket_definition
 
         # A dictionary of categories, each with its own bucket structure where buckets are filled with dates from that particular category, according to the bucket definition
-        self._bucket_Structure = {category: {bucket: [] for bucket in range(0, len(self.bucket_Definition))} for category in range(self._categories)}
+        self._bucket_Structure = {
+            category: {bucket: [] for bucket in range(0, len(self.bucket_Definition))}
+            for category in range(self._categories)
+        }
         self._lookup_Structure = {}
 
         # Fills the bucket_Structure
@@ -96,7 +113,6 @@ class Bucketer:
     def lookup_Structure(self):
         return self._lookup_Structure
 
-
     def bucket_of(self, start_of_draw_period):
         """
         Associates a set of ``draw_Period`` days with a bucket number. Uses memoization to speed up lookup of previously determined bucket numbers.
@@ -116,23 +132,27 @@ class Bucketer:
 
             # Adding to the start of draw period to allow for accurate bucketing of draw periods that are on the border between buckets
             # This also allows us to obtain a leap day when needed, without compromising the ShortDate functionality
-            days_offset = int(self.draw_Period / 2) 
-            offset_draw_period = start_of_draw_period + datetime.timedelta(days = days_offset)
+            days_offset = int(self.draw_Period / 2)
+            offset_draw_period = start_of_draw_period + datetime.timedelta(days=days_offset)
 
             # This date is in the bucket that loops around the list, spanning two years
-            if (offset_draw_period < self.bucket_Definition[start_index] or offset_draw_period >= self.bucket_Definition[end_index]):
+            if (
+                offset_draw_period < self.bucket_Definition[start_index]
+                or offset_draw_period >= self.bucket_Definition[end_index]
+            ):
                 return len(self.bucket_Definition) - 1
 
             # Binary lookup in sorted bucket_Definition otherwise
             while start_index != end_index:
-
                 middle_index = start_index + int((end_index - start_index) / 2)
                 search_target_start = self.bucket_Definition[middle_index]
-                search_target_end = self.bucket_Definition[middle_index + 1] # We can do this without fear of IndexException because we have eliminated this case in the if statement above the while block
+                search_target_end = self.bucket_Definition[
+                    middle_index + 1
+                ]  # We can do this without fear of IndexException because we have eliminated this case in the if statement above the while block
 
-                if (offset_draw_period >= search_target_start):
+                if offset_draw_period >= search_target_start:
                     start_index = middle_index
-                    if (offset_draw_period < search_target_end):
+                    if offset_draw_period < search_target_end:
                         return middle_index
                 else:
                     end_index = middle_index
@@ -158,13 +178,21 @@ class Bucketer:
         """
         raise NotImplementedError
 
-    
     def populate_Buckets(self):
         """
         Utility function that runs once in the constructor, saving all relevant dates in their appropriate buckets, filling the ``bucket_Structure`` accordingly.
         """
         # We run through all available historical data (bar the last days that would not fit if we started a draw period then)
-        for date in [self.historical_Start_Date + datetime.timedelta(days) for days in range((self.historical_End_Date - datetime.timedelta(self.draw_Period - 2) - self.historical_Start_Date).days)]:
+        for date in [
+            self.historical_Start_Date + datetime.timedelta(days)
+            for days in range(
+                (
+                    self.historical_End_Date
+                    - datetime.timedelta(self.draw_Period - 2)
+                    - self.historical_Start_Date
+                ).days
+            )
+        ]:
             # Getting the right list in the bucket structure:
             bucket = self.bucket_of(date)
             category = self.assign_Category(date)
@@ -173,10 +201,11 @@ class Bucketer:
             self.bucket_Structure[category][bucket].append(date)
 
         # Picking off where we left off the last loop to fill the table with future dates as well:
-        for future_date in [date + datetime.timedelta(days + 1) for days in range((datetime.datetime.today() + datetime.timedelta(365 * 6) - date).days)]:
-
+        for future_date in [
+            date + datetime.timedelta(days + 1)
+            for days in range((datetime.datetime.today() + datetime.timedelta(365 * 6) - date).days)
+        ]:
             self.lookup_Structure[future_date] = self.tabulate_Date_Info(future_date)
-
 
     # Called by the simulation engine with the datetime.datetime edges of the future period as parameters;
     # Returns a list of randomly selected datetime.datetime objects that would fill the future period if sampled draw_Period-days at a time starting at each of them and stiching it all together:
@@ -194,9 +223,11 @@ class Bucketer:
         """
 
         sample = []
-        date = future_start_date 
+        date = future_start_date
 
-        while date <= future_end_date - datetime.timedelta(self.draw_Period - 1): # Still in range for a full draw period remaining
+        while date <= future_end_date - datetime.timedelta(
+            self.draw_Period - 1
+        ):  # Still in range for a full draw period remaining
             # Getting the right list in the bucket structure:
             bucket = self.bucket_of(date)
             category = self.assign_Category(date)
