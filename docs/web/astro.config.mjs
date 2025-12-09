0613a3b8169b78a93bc29ee419879f3f9eb8cd5e
imharrisonking <harrison.k@hotmail.co.uk>
2025-12-09 11:42:31 +0000

    Fixes typescript alias paths

--- docs/web/astro.config.mjs
+++ docs/web/astro.config.mjs
@@ -5,6 +5,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                '@': new URL('./src', import.meta.url).pathname
+            }
+        }
+    },
     integrations: [
         starlight({
             title: "Low Carbon Contracts",
